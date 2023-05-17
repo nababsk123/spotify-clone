7104e61144c4a0be14657ca79c4fcf6510ff9a63
nababsk123 <124558435+nababsk123@users.noreply.github.com>
2023-05-17 11:19:46 +0530

    music player app done

---
+++ spotify clone  app/spotify-clone/build/precache-manifest.3f1a36ed629e37929ff118e5c0e0df1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52c2127414e58ee0e46de9690887da1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a77ff7b9894bdd8f66cd",
+    "url": "/static/css/main.2ec7f281.chunk.css"
+  },
+  {
+    "revision": "947542b8782f53767f34",
+    "url": "/static/js/2.bc02c531.chunk.js"
+  },
+  {
+    "revision": "b1cb8eb1b985c13bdc6256936eb9486c",
+    "url": "/static/js/2.bc02c531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a77ff7b9894bdd8f66cd",
+    "url": "/static/js/main.3c5379ab.chunk.js"
+  },
+  {
+    "revision": "65a997ae0f9bcada24e4",
+    "url": "/static/js/runtime-main.8830081d.js"
+  }
+]);
---
+++ spotify clone  app/spotify-clone/src/spotify.js
@@ -0,0 +1,28 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "6fdbbb5520294ec1a9ddbefd8dc235f1";
+const redirectUri = "http://spotify-clone-4f3d6.web.app";
+const scope = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
